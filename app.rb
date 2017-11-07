022de24e233ba80533906d1c5c95db03b1ef939c
msalam <ms17alam@gmail.com>
2017-11-06 21:35:53 -0500

    have just created business website with sinatra

---
+++ app.rb
@@ -0,0 +1,48 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  erb :index
+end
+get '/index' do
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+get '/ship' do
+  erb :ship
+end
+
+get '/team' do
+  erb :team
+end
+get '/contact' do
+  erb :contact
+end
+
+
+
+
+
+
+
+
+
+post '/' do
+
+from = Email.new(email: params['email'])
+ to = Email.new(email: 'ferare2007@gmail.com')
+ subject = 'Sending with SendGrid is Fun'
+ content = Content.new(type: 'text/plain', value: params['body'])
+ mail = Mail.new(from, subject, to, content)
+ sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+ response = sg.client.mail._('send').post(request_body: mail.to_json)
+ puts response.status_code
+ puts response.body
+ puts response.headers
+
+
+  erb :index
+end
